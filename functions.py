ca919a971c86de98982b6c50d53685d8364234a0
Cesar Lopes <cesargl@sebraesp.com.br>
2024-06-14 14:06:59 -0300

    commitando: arquivos relacionados

---
+++ functions.py
@@ -0,0 +1,4 @@
+#Definindo funçoes essencias para rodar APP
+def DTref():
+   resp = input('Qual a data de referencia? (ex:12/01/2024) ')
+   
---
+++ main.py
@@ -0,0 +1,22 @@
+#importando libs
+
+import pandas as pd
+from pandas import DataFrame
+from PyPDF2 import PdfReader
+
+#importando arquivos
+from functions import DTref
+
+x = 1
+#caminho = "C:/Users/cesargl/OneDrive - SERVICO DE APOIO AS MICRO E PEQUENAS EMPRESAS DE SAO PAULO - SEBRAE/.git/scriptsSebrae_/teste_arquivos"
+caminho = r"C:\Users\cesargl\OneDrive - SERVICO DE APOIO AS MICRO E PEQUENAS EMPRESAS DE SAO PAULO - SEBRAE\Contratos"
+
+def main():
+ if x is not None:
+    try:
+        DTref()
+    except Exception as e:
+         print("erro") 
+
+if __name__ == "__main__":
+      main()    
