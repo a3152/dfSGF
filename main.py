e5e030bf0c90675f22c8c758d4753f0d4111aa62
Cesar Lopes <cesargl@sebraesp.com.br>
2024-06-14 17:02:31 -0300

    update: atualiza main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #importando libs
 
-import pandas as pd
 from pandas import DataFrame
 from PyPDF2 import PdfReader
 
 #importando arquivos
 from functions import DTref
+
+#definindo caminho para busca de arquivos
 
 x = 1
 #caminho = "C:/Users/cesargl/OneDrive - SERVICO DE APOIO AS MICRO E PEQUENAS EMPRESAS DE SAO PAULO - SEBRAE/.git/scriptsSebrae_/teste_arquivos"
